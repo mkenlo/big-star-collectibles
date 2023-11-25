5e102909d9b26909a61c48cfba8d966eaf0a560f
Melanie <mel.lontsi@gmail.com>
2023-11-25 07:11:43 -0800

    learning Spring MVC, setting up the app and setting up the Home page flow

---
+++ src/main/java/com/example/collectibles/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.collectibles.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String displayHome(){
+        return "index";
+    }
+
+    @GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName){
+        return "/characters/"+charName;
+    }
+
+}
